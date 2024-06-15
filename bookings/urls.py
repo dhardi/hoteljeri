5de929a4c0c57bba241e641a09766cd80b426cab
dhardi <denishardi10@outlook.com>
2024-06-15 11:35:55 +0000

    fix standards pip8 url

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,16 +1,24 @@
-from . import views 
+from . import views
 from django.urls import path
-from .views import book_room, booking_success, index
+from .views import (
+    book_room,
+    booking_success,
+    index,
+)
 
 urlpatterns = [
-    path('', index, name='bookings'),  
+    path('', index, name='bookings'),
     path('booking-success/', booking_success, name='booking_success'),
     path('submit_review/', views.submit_review, name='submit_review'),
-    path('booking-success/<str:amount>/', booking_success, name='booking_success_amount'), 
+    path('booking-success/<str:amount>/', 
+         booking_success, 
+         name='booking_success_amount'),
     path('my-bookings/', views.user_bookings, name='user_bookings'),
-    path('my-bookings/change/<int:pk>/', views.change_booking, name='change_booking'),
-    path('my-bookings/delete/<int:pk>/', views.delete_booking, name='delete_booking'),
+    path('my-bookings/change/<int:pk>/', 
+         views.change_booking, 
+         name='change_booking'),
+    path('my-bookings/delete/<int:pk>/', 
+         views.delete_booking, 
+         name='delete_booking'),
     path('book-room/', book_room, name='book_room'),
 ]
-    
-
