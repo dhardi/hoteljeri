7c995c0ab665f14ea438e3d8dea0e3d2917c2a23
dhardi <denishardi10@outlook.com>
2024-06-15 10:20:42 +0000

    fix test view and booking

--- bookings/tests.py
+++ bookings/tests.py
@@ -9,7 +9,7 @@ class RoomBookingViewTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='12345')
-        self.url = reverse('index')  # Ajuste o nome conforme necessário
+        self.url = reverse('bookings')
 
     def test_booking_page_not_authenticated(self):
         response = self.client.get(self.url)
@@ -37,28 +37,30 @@ class RoomBookingViewTest(TestCase):
 class BookingTestCase(TestCase):
 
     def setUp(self):
+        self.client = Client()
         self.user = User.objects.create_user(username='testuser2', password='12345')
+        self.client.login(username='testuser2', password='12345')
         self.room = Room.objects.create(name='Room 101')
 
     def test_update_booking(self):
         booking = Booking.objects.create(user=self.user, room=self.room, start_time=datetime.now(), end_time=datetime.now() + timedelta(hours=1))
-        
-        # Simular um pequeno atraso para garantir que o tempo de atualização seja diferente
-        new_end_time = datetime.now() + timedelta(hours=2)
-        response = self.client.post(reverse('update_booking', kwargs={'pk': booking.pk}), {'end_time': new_end_time})
+        new_end_time = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+        response = self.client.post(reverse('change_booking', kwargs={'pk': booking.pk}), {
+            'room': self.room.pk,
+            'start_time': booking.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'end_time': new_end_time
+        })
+
+        self.assertEqual(response.status_code, 302)  # Redirecionamento após atualização bem-sucedida
+
         updated_booking = Booking.objects.get(pk=booking.pk)
-        
-        # Recarregar o objeto da base de dados para garantir que estamos usando os mesmos dados
-        updated_booking.refresh_from_db()
-        
-        # Verificar se o tempo de fim foi atualizado
-        self.assertGreater(updated_booking.end_time, booking.end_time)
+        self.assertEqual(updated_booking.end_time.strftime('%Y-%m-%d %H:%M:%S'), new_end_time)
 
     def test_delete_booking(self):
         booking = Booking.objects.create(user=self.user, room=self.room, start_time=datetime.now(), end_time=datetime.now() + timedelta(hours=1))
         response = self.client.post(reverse('delete_booking', kwargs={'pk': booking.pk}))
         self.assertEqual(response.status_code, 302)  # Redirecionamento após exclusão bem-sucedida
-        
+
         # Verifica se o Booking foi excluído do banco de dados
         booking_exists = Booking.objects.filter(pk=booking.pk).exists()
         self.assertFalse(booking_exists, f"Booking with pk={booking.pk} still exists in the database.")
