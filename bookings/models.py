5e7ecb32c094fdd193b7c6c6638358a6b49de21d
dhardi <denishardi10@outlook.com>
2024-06-17 14:43:07 +0000

    reformat standard pip8 tests and models

--- bookings/models.py
+++ bookings/models.py
@@ -2,37 +2,46 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-#room model
+
+# Room model
 class Room(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.IntegerField(null=True, blank=True)
     image_url_main = models.URLField(blank=True)
-    description = models.TextField(blank=True, default='')  
-    price_per_night = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    description = models.TextField(blank=True, default='')
+    price_per_night = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00
+    )
     small_image_url_1 = models.URLField(blank=True)
     small_image_url_2 = models.URLField(blank=True)
     small_image_url_3 = models.URLField(blank=True)
- 
-#Booking model
+
+
+# Booking model
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    total_price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00
+    )
     created_at = models.DateTimeField(default=timezone.now)
-    
 
     def duration(self):
         return self.end_time - self.start_time
 
-#Review model
+
+# Review model
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 6)])
+    rating = models.PositiveSmallIntegerField(
+        choices=[(i, i) for i in range(1, 6)]
+    )
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Review by {self.user.username} - {self.rating} stars'
+
 
--- bookings/tests.py
+++ bookings/tests.py
@@ -4,18 +4,23 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 from .models import Room, Booking
 
+
 class RoomBookingViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.user = User.objects.create_user(
+            username='testuser', password='12345'
+        )
         self.url = reverse('bookings')
 
     def test_booking_page_not_authenticated(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<div class="container mt-5">')
-        self.assertContains(response, '<div class="alert alert-warning" role="alert">')
+        self.assertContains(
+            response, '<div class="alert alert-warning" role="alert">'
+        )
         self.assertContains(response, 'Please <a href="')
         self.assertContains(response, '">sign in</a> to book a room.')
         self.assertContains(response, '</div>')
@@ -25,42 +30,74 @@ class RoomBookingViewTest(TestCase):
         self.client.login(username='testuser', password='12345')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertNotContains(response, "Please <a href=\"/login/\">sign in</a> to book a room.")
+        self.assertNotContains(
+            response, 
+            "Please <a href=\"/login/\">sign in</a> to book a room."
+        )
         self.assertContains(response, '<h1>Book a Room</h1>')
 
     def test_booking_page_with_form_errors(self):
         self.client.login(username='testuser', password='12345')
-        response = self.client.post(self.url, {'room': '', 'start_time': '', 'end_time': ''})
+        response = self.client.post(
+            self.url, {'room': '', 'start_time': '', 'end_time': ''}
+        )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Oops! There was an error.')
+
 
 class BookingTestCase(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser2', password='12345')
+        self.user = User.objects.create_user(
+            username='testuser2', password='12345'
+        )
         self.client.login(username='testuser2', password='12345')
         self.room = Room.objects.create(name='Room 101')
 
     def test_update_booking(self):
-        booking = Booking.objects.create(user=self.user, room=self.room, start_time=datetime.now(), end_time=datetime.now() + timedelta(hours=1))
-        new_end_time = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
-        response = self.client.post(reverse('change_booking', kwargs={'pk': booking.pk}), {
-            'room': self.room.pk,
-            'start_time': booking.start_time.strftime('%Y-%m-%d %H:%M:%S'),
-            'end_time': new_end_time
-        })
+        booking = Booking.objects.create(
+            user=self.user,
+            room=self.room,
+            start_time=datetime.now(),
+            end_time=datetime.now() + timedelta(hours=1)
+        )
+        new_end_time = (
+            datetime.now() + timedelta(days=1)
+        ).strftime('%Y-%m-%d %H:%M:%S')
+        response = self.client.post(
+            reverse('change_booking', kwargs={'pk': booking.pk}),
+            {
+                'room': self.room.pk,
+                'start_time': booking.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+                'end_time': new_end_time
+            }
+        )
 
-        self.assertEqual(response.status_code, 302)  # check if update has been done
+        self.assertEqual(response.status_code, 302)
 
         updated_booking = Booking.objects.get(pk=booking.pk)
-        self.assertEqual(updated_booking.end_time.strftime('%Y-%m-%d %H:%M:%S'), new_end_time)
+        self.assertEqual(
+            updated_booking.end_time.strftime('%Y-%m-%d %H:%M:%S'), 
+            new_end_time
+        )
 
     def test_delete_booking(self):
-        booking = Booking.objects.create(user=self.user, room=self.room, start_time=datetime.now(), end_time=datetime.now() + timedelta(hours=1))
-        response = self.client.post(reverse('delete_booking', kwargs={'pk': booking.pk}))
-        self.assertEqual(response.status_code, 302)  # check if redirecit was done 
+        booking = Booking.objects.create(
+            user=self.user,
+            room=self.room,
+            start_time=datetime.now(),
+            end_time=datetime.now() + timedelta(hours=1)
+        )
+        response = self.client.post(
+            reverse('delete_booking', kwargs={'pk': booking.pk})
+        )
+        self.assertEqual(response.status_code, 302)
 
-        # check if book as delete from db
+        # check if booking was deleted from db
         booking_exists = Booking.objects.filter(pk=booking.pk).exists()
-        self.assertFalse(booking_exists, f"Booking with pk={booking.pk} still exists in the database.")
+        self.assertFalse(
+            booking_exists,
+            f"Booking with pk={booking.pk} still exists in the database."
+        )
+
