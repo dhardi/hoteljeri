88b2d867be2dd74bc1e475442b6a4f54da6e7e91
dhardi <denishardi10@outlook.com>
2024-06-23 12:13:27 +0000

    change secret key for env file

--- hotel_jeri/settings.py
+++ hotel_jeri/settings.py
@@ -26,7 +26,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)o_ojh%i7*(hv3xzw+)9@-orv&^i(f7d=0)5^&&)yk!3w+y*f5"
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
